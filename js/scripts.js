69deab5689165ff21578ff0210616de00180d699
Nicholas LiCalzi <nicholaslicalzi@gmail.com>
2019-04-11 10:12:26 -0400

    updating layout

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,17 @@ $(document).ready(function(){
 $("#show").click(function(){
   $("div").show();
  });
+
+$('#tentacleSubmit').on('click', function() {
+  var tentacles=$('tentacles').val();
+  console.log('it read  this from the input', tentacles)
+});
+
+
+var name = 'Chris'; // String
+
+// var age = 38; // int
+// instead of hardcoding age in javascript, we can read it from an input
+
+
+console.log(tentacles)
