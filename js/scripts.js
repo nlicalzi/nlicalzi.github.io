e8443db0aa863eb90910a6aaed2a01ca67aa359c
Nicholas LiCalzi <nicholaslicalzi@gmail.com>
2019-04-11 18:40:14 -0400

    final update before submission

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+function myFunction() {
+  alert("💢 Aha! No projects to display yet, sadly :( 💢");
+}
+
 $(document).ready(function() {
   $("button").click(function() {
     $(".dots").toggleClass("alt_background"
