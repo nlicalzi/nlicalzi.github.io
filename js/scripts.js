4929a98d256673eca3aae3da7bf7182fa6fed481
nlicalzi <nicholas.licalzi@gmail.com>
2021-01-06 11:23:28 -0500

    simplifying before building back up

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-  alert("💢 Aha! No projects to display yet, sadly :( 💢");
-};
-
-$(document).ready(function() {
-  $("button").click(function() {
-    $(".dots").toggleClass("alt_background");
-  });
-});
