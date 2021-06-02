05cf97206eaee30d56150f3f54b8beb7d1fc44e2
nlicalzi <nicholas.licalzi@gmail.com>
2021-06-02 17:19:19 -0400

    replaced website

--- README.md
+++
@@ -1,3 +0,0 @@
-#nyu-2019-class2
-
-A directory to contain baby's first GitHub hosted webpage
--- app.js
+++ app.js
@@ -4,4 +4,4 @@ const port = 3000;
 
 app.use(express.static('public'));
 
-app.listen(port, () => console.log('Running your sweet app!'));
+app.listen(port, () => console.log());
